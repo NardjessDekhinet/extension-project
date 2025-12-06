e054c6085d11bae129d78bd5498a838bbaf26df6
unknown <n_dekhinet@estin.dz>
2025-12-06 05:53:34 +0100

    added dockerignore

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('../server');
-
+//unit test with jest 
 describe('GET /', () => {
   it('responds with JSON message', async () => {
     const res = await request(app).get('/');
